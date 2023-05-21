e149eef39fcc244b1c0db41903c9c0b964f37e2a
MarkParker5 <markparker.it@gmail.com>
2023-05-21 23:10:25 +0200

    add examples [no ci]

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rpi-reactive-gpio
 
 Syntax sugar for controlling RPi.GPIO with reactive design.
+
+TODO: OOP style
--- examples.py
+++ examples.py
@@ -1,6 +1,5 @@
 import time
 import RPi.GPIO as GPIO
-from rpi_reactive_gpio.leds import LedManager, RGBLedManager
 
 
 def test_all_pins():
@@ -24,3 +23,58 @@ def test_all_pins():
         GPIO.output(pin, GPIO.HIGH)
         time.sleep(0.5)
         GPIO.output(pin, GPIO.LOW)
+
+
+from rpi_reactive_gpio.leds import ButtonClick, LedManager, RGBLedManager, LedState, RGBLedState, main_loop
+
+
+led_mode = 0
+color_index = 0
+    
+@ButtonClick(pin = 5)
+def button_click(num_clicks: int):
+    global led_mode
+    if num_clicks == 1:
+        led_mode = int(not bool(led_mode))
+    elif num_clicks == 2:
+        led_mode = 2
+    elif num_clicks == 3:
+        led_mode = 3
+    else:
+        print(f'button2 clicked {num_clicks} times')
+    
+@ButtonClick(pin = 5, debounce_time_ms = 2000)
+def button_long_press(_):
+    color_index += 1
+    
+@LedManager(pin = 29)
+def get_led_state() -> LedState:
+    global led_mode
+    match led_mode:
+        case 0:
+            return LedState.off
+        case 1:
+            return LedState.on
+        case 2:
+            return LedState.blink
+        case 3:
+            return LedState.fast_blink
+
+@RGBLedManager(red_pin = 33, green_pin = 35, blue_pin = 37)
+def get_rgb_led_state() -> RGBLedState:
+    global color_index
+    match color_index % 5:
+        case 0:
+            return RGBLedState.off
+        case 1:
+            return RGBLedState.red
+        case 2:
+            return RGBLedState.yellow
+        case 3:
+            return RGBLedState.green
+        case 4:
+            return RGBLedState.blue
+        
+if __name__ == '__main__':
+    test_all_pins()
+    main_loop()
