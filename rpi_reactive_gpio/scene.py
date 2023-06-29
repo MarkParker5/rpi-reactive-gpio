31b9a441dc9f3fecc9bb44a83af2c74375654a81
Mark Parker <mark@parker-programs.com>
2023-06-29 17:41:29 +0200

    add Scene.stop

--- rpi_reactive_gpio/scene.py
+++ rpi_reactive_gpio/scene.py
@@ -5,7 +5,12 @@ from .buttons import ButtonClick
 
 
 class Scene(ABC):
+    
+    _is_running = False
+    
     def main_loop(self, update_interval: float = 1.0, ticks_count: int = 100):
+        self._is_running = True
+        
         attributes = [getattr(self, a) for a in dir(self)]
         self.managers = filter(lambda a: isinstance(a, PinManagerProtocol), attributes)
         self.tickables = filter(lambda a: isinstance(a, Tickable), attributes)
@@ -14,17 +19,20 @@ class Scene(ABC):
         for event in self.events:
             event.pass_args = [self]
         
-        while True:
-            self.update_all()
+        while self._is_running:
+            self._update_all()
             for i in range(ticks_count):
-                self.tick_all(i / ticks_count)
+                self._tick_all(i / ticks_count)
                 time.sleep(update_interval / ticks_count)
                 
-    def update_all(self):
+    def stop(self):
+        self._is_running = False
+                
+    def _update_all(self):
         for manager in self.managers:
             manager.update(self)
             
-    def tick_all(self, period: float = 1.0):
+    def _tick_all(self, period: float = 1.0):
         '''period must be in range 0.0...1.0'''
         for tickable in self.tickables:
             tickable.tick(period)
